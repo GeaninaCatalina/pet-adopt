e3ea9c9d15c7e64e27832529494a2e88f6b80576
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-09-01 20:59:24 +0200

    Add eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
+import Pet from '../views/Pet.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +13,25 @@ const routes = [
     name: 'Home',
     component: Home
   },
+
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    name: 'Cats',
+    component: Cats
+  },
+
+  {
+    path: '/',
+    name: 'Dogs',
+    component: Dogs
+  },
+
+  {
+    path: '/',
+    name: 'Pet',
+    component: Pet
+  },
+
 ]
 
 const router = new VueRouter({
