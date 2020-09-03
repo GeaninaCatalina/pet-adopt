4eee94684af7bdb0d4517a2f357db2cb180b21f7
GeaninaCatalina <masgras.geanina@hotmail.com>
2020-09-03 15:19:52 +0200

    -M

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <NewComp msg="Welcome to Your Vue.js App"/>
+  <div>
+    Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import NewComp from '@/components/NewComp.vue'
 
 export default {
   name: 'Home',
-  components: {
-    NewComp
-  }
+  
 }
 </script>
